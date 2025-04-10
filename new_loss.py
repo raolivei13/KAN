5830b377d56eac270aa86231545e33201893019f
Richard Oliveira <77032829+raolivei13@users.noreply.github.com>
2025-04-10 06:55:46 -0700

    Update new_loss.py

--- new_loss.py
+++ new_loss.py
@@ -15,7 +15,7 @@ class CombinedMSESSIMLoss(nn.Module):
         self.ssim_weight = ssim_weight
 
     def forward(self, output, target):
-        # Compute the standard MSE loss
+        
         loss_mse = self.mse_loss(output, target)
 
 
@@ -23,11 +23,9 @@ class CombinedMSESSIMLoss(nn.Module):
         output_img = output.view(batch_size, 1, 28, 28)
         target_img = target.view(batch_size, 1, 28, 28)
 
-        # Compute SSIM. Ensure your images are normalized appropriately,
-        # typically in the [0, 1] range.
+        
         ssim_val = ssim(output_img, target_img, data_range=target.max() - target.min(), size_average=True)
-        # Convert SSIM to a loss. Since a higher SSIM means better similarity,
-        # subtract from 1 to use it as a loss.
+        
         loss_ssim = 1 - ssim_val
 
         total_loss = (self.mse_weight * loss_mse) + (self.ssim_weight * loss_ssim)
